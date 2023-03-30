79fbbb7390f98693d495cb863cce1994860f6d18
sumon-hossain <sumonengr.96@gmail.com>
2023-03-30 16:36:28 +0600

    Fetching data-2

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,22 +1,31 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState("random dad joke");
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      const { data } = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      setJoke(data.joke);
+    } catch (error) {
+      console.log(error.resp);
+    }
   };
 
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke bg-green-300 py-10 px-5 text-2xl">{joke}</p>
     </section>
   );
 };
