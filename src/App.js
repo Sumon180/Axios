b6b14fe9c0b10e39c24effe2f5842ed5a86c1e42
sumon-hossain <sumonengr.96@gmail.com>
2023-03-30 16:19:53 +0600

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/1-first-request";
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,24 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      const data = response.data
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
