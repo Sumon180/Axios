1adc0a66ae7a77958a4e38be47f8b5c7005996b1
sumon-hossain <sumonengr.96@gmail.com>
2023-03-30 16:59:59 +0600

    Post Request

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -1,45 +1,52 @@
-import { useState } from 'react';
-import axios from 'axios';
-const url = 'https://course-api.com/axios-tutorial-post';
+import { useState } from "react";
+import axios from "axios";
+const url = "https://course-api.com/axios-tutorial-post";
 
 const PostRequest = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+      const resp = await axios.post(url, { name, email });
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error.resp);
+    }
   };
 
   return (
     <section>
-      <h2 className='text-center'>post request</h2>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <h2 className="text-center">post request</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
           <input
-            type='text'
-            className='form-input'
-            id='name'
+            type="text"
+            className="form-input"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
         </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             email
           </label>
           <input
-            type='email'
-            className='form-input'
-            id='email'
+            type="email"
+            className="form-input"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           login
         </button>
       </form>
