7a09be4f341c853cc9296126281bef0ad0044446
sumon-hossain <sumonengr.96@gmail.com>
2023-03-30 20:45:44 +0600

    Interceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
-
-const url = 'https://course-api.com/react-store-products';
+import { useEffect } from "react";
+import authFetch from "../axios/custom";
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch("/react-store-products");
+      console.log(resp);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
